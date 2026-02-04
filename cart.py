f900d9f6ce5e89b78e293f5b6a750666f65101c6
Ka666if <ka666if@mail.ru>
2026-02-05 03:09:33 +0500

    Implement ShoppingCart class with add/remove methods

---
+++ cart.py
@@ -0,0 +1,17 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, product):
+        if product.is_available():
+            self.items.append(product)
+            product.quantity -= 1
+        else:
+            print(f"Товар {product.name} отсутствует на складе")
+
+    def remove_item(self, product):
+        if product in self.items:
+            self.items.remove(product)
+            product.quantity += 1
+
+
